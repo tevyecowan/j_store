1497f0a21bb8c7e798aa6f475e611eeaced0015a
Tevye Cowan <t.b.cowan@gmail.com>
2019-04-11 12:17:23 -0700

    update navbar

--- addDocument.php
+++ addDocument.php
@@ -12,45 +12,11 @@
 <body>
 
 <!-- 					HEADER					-->
-<div id="header">
 
-    <h1 style="text-align: center;">J-Store Online Geniza</h1>
-
-</div>
 <?php
-include('includes/mysqli_connect.php');
+include('includes/header.php');
 ?>
 
-<!-- 					NAV					-->
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown link
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</nav>
 <!-- 					MENU, CONTENT					-->
 <div id="content">
     <?php

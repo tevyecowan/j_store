39c6753c38c5ac84042c50871145cbc34eb7ae17
Tevye Cowan <t.b.cowan@gmail.com>
2019-04-11 18:29:05 -0700

    WIP adding files to database

--- uploads/addDocument.php
+++ uploads/addDocument.php
@@ -20,102 +20,65 @@ include('../includes/mysqli_connect.php');
 
 <!-- 					MENU, CONTENT					-->
 <div id="content">
+
     <?php
     if (isset($_POST['filename'])) {
-
-    $filename = $_POST['filename'];
-    $data  = $_POST['data'];
-
-    $query = "insert into uploads (data, filename)
+        $filename = $_POST['filename'];
+        $data  = $_POST['data'];
+        $query = "insert into uploads (data, filename)
     values('$filename', '$data');";
-
-    if (mysqli_query ($dbc, $query)){
-    //echo "Successfully inserted into Product table<br>";
-    echo "<br>Congrats! You have successfully added your new document.<br>";
-    }else{
-    //	echo "ERROR: Could not able to execute $query. " . mysqli_error($dbc) . "<br>";
-    }
-
+        if (mysqli_query ($dbc, $query)){
+            //echo "Successfully inserted into Product table<br>";
+            echo "<br>Congrats! You have successfully added your new document.<br>";
+        }else{
+            //	echo "ERROR: Could not able to execute $query. " . mysqli_error($dbc) . "<br>";
+        }
     }else{    //if user somehow got to this page without submitting the form
-    echo  "<h4>Please enter your new product into the form below</h4>";
+        echo  "<h4>Please enter your new product into the form below</h4>";
     }
-
     mysqli_close($dbc);
     ?>
-</div>
-<br><br>
+    <?php
+    if(isset($_FILES['image'])){
+        $errors= array();
+        $file_name = $_FILES['image']['name'];
+        $file_size = $_FILES['image']['size'];
+        $file_tmp = $_FILES['image']['tmp_name'];
+        $file_type = $_FILES['image']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
 
-<form name="documentForm" action="addDocument.php" method="POST" enctype="multipart/form-data">
-    File:  <input type="file" name="data"/><br>
-    Document name: <input type='text' name='filename'><br>
-        <input type="submit" name="submit" value="Upload"/>
-</form>
+        $extensions= array("jpeg","jpg","png");
 
-<?php
+        if(in_array($file_ext,$extensions)=== false){
+            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+        }
 
-if (isset($_POST['submit']))
-	{
-		$error_code = $_FILES['data']['error'];
-		if ($error_code) { $list_error = array (1 => 'File size exceeds the maximum allowed',
-												2 => 'File size exceeds the maximum allowed',
-												3 => ' File only partially uploaded ',
-												4 => ' No file was uploaded',
-												6 => 'Temporary folder not found',
-												7 => 'Failed to write file to disk'
-											);
+        if($file_size > 2097152) {
+            $errors[]='File size must be excately 2 MB';
+        }
 
-echo 'ERROR: ' . $list_error[$_FILES['data']['error']];
- } else {
+        if(empty($errors)==true) {
+            move_uploaded_file($file_tmp,"images/".$file_name);
+            echo "Success";
+        }else{
+            print_r($errors);
+        }
+    }
+    mysqli_close($dbc);
+    ?>
 
-if (is_uploaded_file($_FILES['data']['tmp_name'])) {
+    <form action = "addDocument.php" method = "POST" enctype = "multipart/form-data">
+        <input type = "file" name = "image" />
+        <input type = "submit"/>
 
-	// VALIDATION
-	$size = $_FILES['data']['size'];
-	$type = $_FILES['data']['type'];
-	$mime = array('image/jpeg', 'image/jpg');
-	$error = false;
+        <ul>
+            <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
+            <li>File size: <?php echo $_FILES['image']['size'];  ?>
+            <li>File type: <?php echo $_FILES['image']['type'] ?>
+        </ul>
 
-	if ($size > (1024 * 1024)) { // 1 MB
-		$error = 'ERROR: Maximum size allowed is 1 MB';
-	}
-	elseif (!in_array($type, $mime)) {
-		$error = 'ERROR: File type must be JPG or JPEG';
-	}
+    </form>
 
-	// UPLOAD
-	else {
-		$tmp_name = $_FILES['data']['tmp_name'];
-		$new_file = 'files/'.$_FILES['data']['name'];
-		if( move_uploaded_file($tmp_name, $new_file) ){
-			echo 'File successfully uploaded';
-			} else {
-				$error = 'File cannot be uploaded, please try again later';
-				}
-				}
-				// IF ERROR
-				if ($error) {
-					echo $error;
-				}
-			} else {
-				echo 'No file uploaded';
-			}
-		}
-	}
-?>
-
-<!--
-<form name='documentForm' action="addDocument.php" method="POST" >
-    Document image: <input type='file' name='image'><br>
-
-    Document name: <input type='text' name='name'><br>
-
-    Description: <input type='textarea' name='description'><br>
-
-    File: <input type='file' name='file'><br>
-
-    <input type='submit' name='submit'><br>
-
-</form>-->
 </div>
 <!-- 					SCRIPTS					-->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

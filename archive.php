02bd51f661921913450b9e6f6c2d6b1943d5240f
Tevye Cowan <t.b.cowan@gmail.com>
2019-07-04 20:29:49 -0700

    update archive WIP

--- archive.php
+++ archive.php
@@ -24,77 +24,14 @@ $_SESSION['returnPage'] = $returnPage;
 <!-- 					MENU, CONTENT					-->
 <div id="menu">
     <div id="archivePg" style="display: block">
-        <form id="searchForm" action="archive.php" method="post">
-            Search: <input type="text" name="search"><br><br>
-            <?php
-
-            include ('includes/mysqli_connect.php');
-
-            $query = "select * 
-						from Category 
-						order by category_id;";
-
-            $result = mysqli_query ($dbc, $query);
-            echo "<label><b>Narrow results by Category:</b></label><br>";
-            while ($row = mysqli_fetch_array($result)) {
-                echo "<input type=\"checkbox\" name=\"category[]\" value=\"" . $row['category_id'] . "\">" . $row['category_name'] . "</option><br>";
-            }
- 
-            echo "<label><b>Order results by:</b></label><br>";
-            echo "<input type=\"radio\" name=\"orderBy\" value=\"c.category_id\">Category</input><br>";
-            echo "<input type=\"radio\" name=\"orderBy\" value=\"p.product_name\">Name</input><br>";
-            echo "<input type=\"radio\" name=\"orderBy\" value=\"p.price\">Price</input><br>";
-
-            ?>
-            <br> <label><input  type="submit" style="display:none;"> Search </label><br>
-            <?php
-            if (isset($_SESSION['loginName']) && $_SESSION["is_admin"] == "1") {
-                echo "<div id=\"cataAdd\">";
-                echo "<button type=\"button\" id=\"addProdBtn\"; background-color: #FEF497; color: #5228B3;\" onclick=\"window.location.href='addProduct.php?returnPage=".$returnPage."'\">Add Product</button>";
-                echo "</div>";
-            }
-            ?>
-        </form>
         <div id="searchMenuAndBorder">
             <div id="innerWindow">
 
                 <?php
 
-                //Text boxes return an empty string instead of null when empty
-                //This sets search to null if empty
-                if (isset($_POST["search"]) && $_POST["search"] != ''){
-                    $_SESSION['search'] = "'%".$_POST['search']."%'";
-                    $search = $_SESSION['search'];
-                }
-
-                if (isset($_POST["category"])){
-                    $_SESSION['category'] = $_POST['category'];
-                    $category = $_SESSION['category'];
-                }
-
-                if (isset($_POST["orderBy"])){
-                    $_SESSION['orderBy'] = $_POST['orderBy'];
-                    $orderBy = $_SESSION['orderBy'];
-                }
-                $query = "select distinct p.product_id, p.price, p.image, p.product_name, p.description, c.category_id
-						from Product p, product_category c
-						where p.product_id = c.product_id ";
-
-
-                //this sections adds appropate code to the search for each set value
-                if ($search != null){
-                    $query .= " and upper(p.product_name) like upper($search)";
-                }
-
-                if ($category != null){
-                    $query .= " and c.category_id in (" . implode("," ,$category) . ")";
-                }
-
-                if ($orderBy != null){
-                    $query .= " order by $orderBy";
-                }
-
-                $query .= ";";
+                $query = "select distinct filename, description
+						from uploads
+						;";
 
                 //echo $query . "<br>";
 
@@ -102,7 +39,7 @@ $_SESSION['returnPage'] = $returnPage;
                 echo "<div class=\"container\"><div class=\"row\">";
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "<div class=\"card col-lg-3 col-sm-6\">";
-                    echo  "<div class=\"card-body\"><h5 class=\"card-title\">" .  $row['product_name']  . "</h5><p class=\"card-text\"> $" . $row['price']  . "</p><br>";
+                    echo  "<div class=\"card-body\"><h5 class=\"card-title\">" .  $row['filename']  . "</p><br>";
                     echo "<br><br><div class=\"description\">".$row['description']."</div></div>";
                     echo "</div>";
                 }

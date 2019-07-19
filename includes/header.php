6690a7b3f8126bc3930def8cdc09b110d325b1d0
Tevye Cowan <t.b.cowan@gmail.com>
2019-07-18 20:44:46 -0700

    working on header active pills

--- includes/header.php
+++ includes/header.php
@@ -2,16 +2,32 @@
 	session_start();
 	$success = false;
 	$GLOBALS['a'] = 'success';
+
+echo '<script type="text/javascript">	$(\'#indexPg\').click(function () {
+		$(\'#indexPg\').attr("class", "nav-link active");
+		$(\'#aboutPg\').attr("class", "nav-link");
+		$(\'#archivePg\').attr("class", "nav-link");
+	});
+	$(\'#aboutPg\').click(function () {
+		$(\'#aboutPg\').attr("class", "nav-link active");
+		$(\'#indexPg\').attr("class", "nav-link");
+		$(\'#archivePg\').attr("class", "nav-link");
+	});
+	$(\'#archivePg\').click(function () {
+		$(\'#archivePg\').attr("class", "nav-link active");
+		$(\'#aboutPg\').attr("class", "nav-link");
+		$(\'#indexPg\').attr("class", "nav-link");
+	});</script>';
 ?>
 <!-- 					NAV					-->
 <div id="header" class="textContainer cover-container d-flex p-3 mx-auto flex-column">
     <header class="masthead mb-auto">
         <div class="inner">
             <h1 class="masthead-brand">JStore</h1>
-            <nav class="nav nav-masthead justify-content-center">
+            <nav id="navbar" class="nav nav-masthead justify-content-center">
 				<ul class="nav nav-pills">
 					<li class="nav-item">
-					<a class="nav-link active" id="indexPg" href="index.php">Home</a>
+					<a class="nav-link" id="indexPg" href="index.php">Home</a>
 					<li class="nav-item">
 					<a class="nav-link" id="aboutPg" href="about.php">About</a>
 					<li class="nav-item">
--- includes/js.js
+++ includes/js.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-$('#indexPg').click(function () {
-		$("#indexPg").attr("class", "nav-link active");
-		$("#aboutPg").attr("class", "nav-link");
-		$("#archivePg").attr("class", "nav-link");
+	$('#indexPg').click(function () {
+		$('#indexPg').attr("class", "nav-link active");
+		$('#aboutPg').attr("class", "nav-link");
+		$('#archivePg').attr("class", "nav-link");
 	});
 	$('#aboutPg').click(function () {
-		$("#aboutPg").attr("class", "nav-link active");
-		$("#indexPg").attr("class", "nav-link");
-		$("#archivePg").attr("class", "nav-link");
+		$('#aboutPg').attr("class", "nav-link active");
+		$('#indexPg').attr("class", "nav-link");
+		$('#archivePg').attr("class", "nav-link");
 	});
 	$('#archivePg').click(function () {
-		$("#archivePg").attr("class", "nav-link active");
-		$("#aboutPg").attr("class", "nav-link");
-		$("#indexPg").attr("class", "nav-link");
+		$('#archivePg').attr("class", "nav-link active");
+		$('#aboutPg').attr("class", "nav-link");
+		$('#indexPg').attr("class", "nav-link");
 	});
 
 

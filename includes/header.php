13fb733b4a2143252698773792facd2ce8e37804
Tevye Cowan <t.b.cowan@gmail.com>
2019-09-17 13:26:15 -0700

    update header

--- includes/header.php
+++ includes/header.php
@@ -2,22 +2,6 @@
 	session_start();
 	$success = false;
 	$GLOBALS['a'] = 'success';
-
-echo '<script type="text/javascript">	$(\'#indexPg\').click(function () {
-		$(\'#indexPg\').attr("class", "nav-link active");
-		$(\'#aboutPg\').attr("class", "nav-link");
-		$(\'#archivePg\').attr("class", "nav-link");
-	});
-	$(\'#aboutPg\').click(function () {
-		$(\'#aboutPg\').attr("class", "nav-link active");
-		$(\'#indexPg\').attr("class", "nav-link");
-		$(\'#archivePg\').attr("class", "nav-link");
-	});
-	$(\'#archivePg\').click(function () {
-		$(\'#archivePg\').attr("class", "nav-link active");
-		$(\'#aboutPg\').attr("class", "nav-link");
-		$(\'#indexPg\').attr("class", "nav-link");
-	});</script>';
 ?>
 <!-- 					NAV					-->
 <div id="header" class="textContainer cover-container d-flex p-3 mx-auto flex-column">
@@ -27,7 +11,7 @@ echo '<script type="text/javascript">	$(\'#indexPg\').click(function () {
             <nav id="navbar" class="nav nav-masthead justify-content-center">
 				<ul class="nav nav-pills">
 					<li class="nav-item">
-					<a class="nav-link" id="indexPg" href="index.php">Home</a>
+					<a class="nav-link active" id="indexPg" href="index.php">Home</a>
 					<li class="nav-item">
 					<a class="nav-link" id="aboutPg" href="about.php">About</a>
 					<li class="nav-item">
